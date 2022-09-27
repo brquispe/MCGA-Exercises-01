8d438eaa7666838fae673a9e8e3f6ea94702f1e3
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-09-27 20:24:51 -0300

    MCGA-50: Attempt to fix Vercel deploy

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const PORT = process.env.PORT || 3000
-const DATABASE_URL = process.env.DATABASE_URL
+const DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://root:root@cluster0.8ligc.mongodb.net/mcga?retryWrites=true&w=majority'
 
 module.exports = {
   PORT,
