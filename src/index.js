1ae2a9f6716e7fb9c4451f642fcb2d3f6b42a5db
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-08 22:53:12 -0300

    MCGA-53: Allow all origins

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH');
+  next();
+});
+
 app.get('', (req, res) => {
   res.send('<h1>Welcome</h1>');
 });
