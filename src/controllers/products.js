675fd2ee5d308008d1b61b99b07833625bbe8744
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-11 19:43:03 -0300

    MCGA-63: Adapt product  funcs to changes

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,143 +1,160 @@
 const { Product } = require('../models/product');
+const { Provider } = require('../models/provider');
 
 const getProductList = async (req, res) => {
-  const products = await Product.find({ isDeleted: false });
+  const products = await Product.find({ isDeleted: false }).populate({
+    path: 'provider',
+    match: { isDeleted: false },
+    select: { name: true },
+  });
   return res.json({
     message: products.length > 0 ? 'Products found' : 'Products not found',
     data: products,
-    error: false
+    error: false,
   });
 };
 
 const createProduct = async (req, res) => {
   const product = {
-    productId: req.body.productId,
     name: req.body.name,
-    price: req.body.price
+    price: req.body.price,
   };
   try {
     const newProduct = new Product(product);
     const result = await newProduct.save();
-  
+
     return res.status(201).json({
       message: 'Product created!',
       data: result,
-      error: false
+      error: false,
     });
   } catch (error) {
     return res.status(400).json({
       message: error,
       data: product,
-      error: true
-    })
+      error: true,
+    });
   }
 };
 
 const getProduct = async (req, res) => {
   const productId = req.params.productId;
-  const product = await Product.findOne({ productId });
-  if (!product) {
-    return res.status(404).json({
-      message: 'Product not found!',
+  try {
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({
+        message: 'Product not found!',
+        data: undefined,
+        error: true,
+      });
+    }
+
+    return res.json({
+      message: 'Product found',
+      data: product,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error,
       data: undefined,
-      error: true
+      error: true,
     });
   }
-
-  return res.json({
-    message: 'Product found',
-    data: product,
-    error: false
-  });
-}
+};
 
 const updateProduct = async (req, res) => {
   const productId = req.params.productId;
   const newData = {
-    productId: req.body.productId,
     name: req.body.name,
-    price: req.body.price
-  }
-  const product = await Product.findOne({ productId });
-  if (!product) {
-    return res.status(404).json({
-      message: 'Product not found!',
-      data: undefined,
-      error: true
-    });
-  }
-  product.name = newData.name;
-  product.price = newData.price;
+    price: req.body.price,
+    providerId: req.body.providerId,
+  };
   try {
-    const result = await product.save()
-
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({
+        message: 'Product not found!',
+        data: undefined,
+        error: true,
+      });
+    }
+    product.name = newData.name;
+    product.price = newData.price;
+    if (newData.providerId) {
+      const resp = await Provider.findByIdAndUpdate(newData.providerId, {
+        $addToSet: { products: product },
+      });
+      product.provider = resp;
+    }
+    const result = await product.save();
     return res.json({
       message: 'Product updated!',
       data: result,
-      error: false
+      error: false,
     });
   } catch (error) {
     return res.status(400).json({
       message: error,
       data: newData,
-      error: true
-    })
+      error: true,
+    });
   }
-}
+};
 
 const deleteProduct = async (req, res) => {
   const productId = req.params.productId;
-  const product = await Product.findOne({ productId })
-  if (!product) {
-    return res.status(404).json({
-      message: 'Product not found!',
-      data: undefined,
-      error: true
-    });
-  }
-  product.isDeleted = true;
   try {
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({
+        message: 'Product not found!',
+        data: undefined,
+        error: true,
+      });
+    }
+    product.isDeleted = true;
     const result = await product.save();
     return res.status(204).json({
       message: 'Deleted product!',
       data: result,
-      error: false
+      error: false,
     });
   } catch (error) {
     return res.status(400).json({
       message: error,
       data: product,
-      error: true
-    })
+      error: true,
+    });
   }
-}
+};
 
 const activateProduct = async (req, res) => {
   const productId = req.params.productId;
-  const product = await Product.findOne({ productId })
-  if (!product) {
-    return res.status(404).json({
-      message: 'Product not found!',
-      data: undefined,
-      error: true
-    });
-  }
-  product.isDeleted = false;
   try {
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({
+        message: 'Product not found!',
+        data: undefined,
+        error: true,
+      });
+    }
+    product.isDeleted = false;
     const result = await product.save();
     return res.status(204).json({
       message: 'Activated product!',
       data: result,
-      error: false
+      error: false,
     });
   } catch (error) {
     return res.status(400).json({
       message: error,
       data: product,
-      error: true
-    })
+      error: true,
+    });
   }
-}
+};
 
 module.exports = {
   getProductList,
@@ -145,5 +162,5 @@ module.exports = {
   getProduct,
   updateProduct,
   deleteProduct,
-  activateProduct
+  activateProduct,
 };
